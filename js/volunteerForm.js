ff53966b68ef49c3d6d35f5ade87c39a24e12fe4
JustinMelore <melorejustin@gmail.com>
2025-12-17 14:35:40 -0700

    Created element on page to indicate form submissiin status

--- js/volunteerForm.js
+++ js/volunteerForm.js
@@ -1,14 +1,23 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbyyCkxgA2lxbpFvDD18vGlCvDeNVJ1aHi2Bk-rw_ZA-AD41gNAJXRKBkDC4InAvCmaw/exec";
 const form = document.forms[0]
+const submissionStatus = document.getElementById("submitStatus")
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    submissionStatus.textContent = "Submitting Info..."
+    submissionStatus.className = "submitting";
+
     var formData = new FormData(form);
     fetch(scriptURL, { method: "POST", body: formData })
         .then((response) => {
             console.log("Info submitted successfully");
+            submissionStatus.textContent = "Submitted Successfully!"
+            submissionStatus.className = "succeeded"
         })
         .catch((error) => {
             console.error(`Info failed to submit: ${error}`)
+            submissionStatus.textContent = "Submission failed. Please try again"
+            submissionStatus.className = "failed"
         }
     );
 });
